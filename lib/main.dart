9d4be8cb80bc3595d6194911c6a932a2270aa5f4
mayank <mayankrajput40558@gmail.com>
2024-07-31 13:57:25 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/pages/home_page.dart';
+
+void main() {
+  runApp(MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
